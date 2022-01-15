3579c31f7298c52e11aa76531b9874aa5be0e2f4
mohsinahmed22 <ahmed.mohsin98@gmail.com>
2022-01-15 18:20:11 +0500

    Inputbar Component

---
+++ src/Components/Inputbar/Inputbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FiAlignLeft } from 'react-icons/fi';
+
+const Inputbar = () => {
+    return (
+        <>
+           <FiAlignLeft /> test 
+        </> 
+    );
+};
+
+
+export default Inputbar;
